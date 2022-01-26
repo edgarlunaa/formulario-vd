fa3be341d1b82938b66d6f8dbf9f2defd7354ab7
maxiviento <leonmaxi@gmail.com>
2022-01-26 15:45:33 -0300

    formulario de verificacion de obra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule }    from '@angular/common/http';
+
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
